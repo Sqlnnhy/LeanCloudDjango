1132032dff20e0b8a567ceab6cf86803d1efac7f
Sqlnnhy <365472702@qq.com>
2017-08-04 09:41:03 +0800

    Revert: set wsgi

--- wsgi.py
+++ wsgi.py
@@ -9,19 +9,10 @@ from cloud import engine
 from gevent import monkey
 monkey.patch_all()
 
-""" online
 APP_ID = os.environ['LEANCLOUD_APP_ID']
 APP_KEY = os.environ['LEANCLOUD_APP_KEY']
 MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']
 PORT = int(os.environ['LEANCLOUD_APP_PORT'])
-"""
-
-# localhost
-APP_ID = '7sP73D83PYWsCcJVlLDlQUnJ-gzGzoHsz'
-APP_KEY = 'qf16kTDRWbO3KnnTR8y1eYlb'
-MASTER_KEY = 'pkpqn2JuA75Qe16Aos4fH847'
-PORT = 8000
-
 
 leancloud.init(APP_ID, master_key=MASTER_KEY)
 
