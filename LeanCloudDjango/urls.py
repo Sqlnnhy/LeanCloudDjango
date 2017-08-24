72f33916ee50ac5a20f45775048cf3869c039e56
Sqlnnhy <365472702@qq.com>
2017-08-24 14:01:51 +0800

    remove grappelli

--- LeanCloudDjango/urls.py
+++ LeanCloudDjango/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', lcd_view.show),
-    url(r'^home/', lcd_view.home),
+    url(r'index^', lcd_view.show),
+    url(r'^home', lcd_view.home),
     # url(r'^grappelli/', include('grappelli.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
