51ebaebfc83d5eb4e71d55ab947a6d67e47aa1e2
Sqlnnhy <365472702@qq.com>
2019-05-13 21:07:01 +0800

    Update views.py

--- LCD/views.py
+++ LCD/views.py
@@ -31,7 +31,7 @@ def ping(request):
     if request.method == 'POST':
         salt = request.form.get('salt')
     if request.method == 'GET':
-        print(request.GET['salt'])
+        print(request.GET)
         salt = request.GET['salt']
 
     if salt == None or salt == '':
